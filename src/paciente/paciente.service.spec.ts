1dafc1326db13200bca75c18efa4ad67b1f63fb1
nrincon2302 <n.rincon4@uniandes.edu.co>
2024-11-22 09:18:33 -0500

    Prueba

--- src/paciente/paciente.service.spec.ts
+++ src/paciente/paciente.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PacienteService } from './paciente.service';
+import { PacienteEntity } from './paciente.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { faker } from '@faker-js/faker';
 
 describe('PacienteService', () => {
   let service: PacienteService;
+  let repository: Repository<PacienteEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [PacienteService],
     }).compile();
 
     service = module.get<PacienteService>(PacienteService);
+    repository = module.get<Repository<PacienteEntity>>(getRepositoryToken(PacienteEntity));
   });
 
   it('should be defined', () => {
